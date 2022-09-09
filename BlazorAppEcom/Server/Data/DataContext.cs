d4546964db35f0f00a1ca036140f088e0f5c9868
arunav <arunavchetia98@gmail.com>
2022-09-09 22:39:34 +0530

    Add project files.

---
+++ BlazorAppEcom/Server/Data/DataContext.cs
@@ -0,0 +1,39 @@
+﻿namespace BlazorAppEcom.Server.Data
+{
+    public class DataContext : DbContext { 
+        public DataContext(DbContextOptions<DataContext> options): base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            modelBuilder.Entity<Product>().HasData(
+                new Product
+                {
+                    Id = 1,
+                    Title = "2 States: The Story of My Marriage",
+                    Description = "2 States: The Story of My Marriage commonly known as 2 States is a 2009 novel written by Chetan Bhagat.It is the story about a couple coming from two states in India, who face hardships in convincing their parents to approve of their marriage. Bhagat wrote this novel after quitting his job as an investment banker.",
+                    ImageUrl = "https://upload.wikimedia.org/wikipedia/en/3/3a/2_States_-_The_Story_Of_My_Marriage.jpg",
+                    Price = 100
+                },
+                new Product
+                {
+                    Id = 2,
+                    Title = "The 3 Mistakes of My Life",
+                    Description = "The 3 Mistakes of My Life is the third novel written by Chetan Bhagat. The book was published in May 2008 and had an initial print-run of 420,000. The novel follows the story of three friends and is based in the city of Ahmedabad in western India. This is the third best seller novel by Chetan Bhagat.",
+                    ImageUrl = "https://upload.wikimedia.org/wikipedia/en/1/1d/3_Mistakes_of_My_Life.jpg",
+                    Price = 107
+                },
+                new Product
+                {
+                    Id = 3,
+                    Title = "Half Girlfriend",
+                    Description = "Half Girlfriend is an Indian English coming of age, young adult romance novel by Indian author Chetan Bhagat.[1] The novel, set in rural Bihar, New Delhi, Patna, and New York, is the story of a Bihari boy in quest of winning over the girl he loves. This is Bhagat's sixth novel which was released on 1 October 2014[4] by Rupa Publications.",
+                    ImageUrl = "https://upload.wikimedia.org/wikipedia/en/c/c6/Half_Girlfriend.jpg",
+                    Price = 108
+                }
+             );
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }    
+
+}
---
+++ BlazorAppEcom/Server/Migrations/20220909145400_seed.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorAppEcom.Server.Migrations
+{
+    public partial class seed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "Price", "Title" },
+                values: new object[] { 1, "2 States: The Story of My Marriage commonly known as 2 States is a 2009 novel written by Chetan Bhagat.It is the story about a couple coming from two states in India, who face hardships in convincing their parents to approve of their marriage. Bhagat wrote this novel after quitting his job as an investment banker.", "https://upload.wikimedia.org/wikipedia/en/3/3a/2_States_-_The_Story_Of_My_Marriage.jpg", 100m, "2 States: The Story of My Marriage" });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "Price", "Title" },
+                values: new object[] { 2, "The 3 Mistakes of My Life is the third novel written by Chetan Bhagat. The book was published in May 2008 and had an initial print-run of 420,000. The novel follows the story of three friends and is based in the city of Ahmedabad in western India. This is the third best seller novel by Chetan Bhagat.", "https://upload.wikimedia.org/wikipedia/en/1/1d/3_Mistakes_of_My_Life.jpg", 107m, "The 3 Mistakes of My Life" });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "Price", "Title" },
+                values: new object[] { 3, "Half Girlfriend is an Indian English coming of age, young adult romance novel by Indian author Chetan Bhagat.[1] The novel, set in rural Bihar, New Delhi, Patna, and New York, is the story of a Bihari boy in quest of winning over the girl he loves. This is Bhagat's sixth novel which was released on 1 October 2014[4] by Rupa Publications.", "https://upload.wikimedia.org/wikipedia/en/c/c6/Half_Girlfriend.jpg", 108m, "Half Girlfriend" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
